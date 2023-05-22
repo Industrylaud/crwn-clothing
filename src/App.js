3836f119eaf88cf8d1006644847280c6bbc9079d
Industrylaud <mrfros22@gmail.com>
2023-05-22 23:01:31 +0200

    Add basic navbar

--- src/App.js
+++ src/App.js
@@ -1,17 +1,7 @@
 import { Route, Routes, Outlet } from 'react-router-dom';
 
+import Navigation from './routes/navigation/navigation.component';
 import Home from './routes/home/home.component';
-
-const Navigation = () => {
-	return (
-		<div>
-			<div>
-				<h1>I am the navigation bar</h1>
-			</div>
-			<Outlet />
-		</div>
-	);
-};
 
 const App = () => {
 	return (
