a96a43b1d19440164fb3d077a735628d57afaf77
Industrylaud <mrfros22@gmail.com>
2023-05-23 23:17:40 +0200

    add creating users in DB

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -30,10 +30,26 @@ export const db = getFirestore();
 
 export const createUserDocumentFromAuth = async (userAuth) => {
 	const userDocRef = doc(db, 'users', userAuth.uid);
-
-	console.log(userDocRef);
-
 	const userSnapshot = await getDoc(userDocRef);
 	console.log(userSnapshot);
 	console.log(userSnapshot.exists());
+
+	if (userSnapshot.exists()) {
+		return userDocRef;
+	}
+
+	const { displayName, email } = userAuth;
+	const createdAt = new Date();
+
+	try {
+		await setDoc(userDocRef, {
+			displayName,
+			email,
+			createdAt,
+		});
+	} catch (error) {
+		console.error('error creating the user', error.message);
+	}
+
+	return userDocRef;
 };
