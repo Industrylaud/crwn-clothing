20a0e39e128be0976c37fc3dde639f6b3350e478
Industrylaud <mrfros22@gmail.com>
2023-05-24 23:36:24 +0200

    add user sign up

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,5 +1,10 @@
 import { useState } from 'react';
 
+import {
+	createAuthUserWithEmailAndPassword,
+	createUserDocumentFromAuth,
+} from '../../utils/firebase/firebase.utils';
+
 const defaultFormfields = {
 	displayName: '',
 	email: '',
@@ -11,6 +16,37 @@ const SignUpForm = () => {
 	const [formFields, setformFields] = useState(defaultFormfields);
 	const { displayName, email, password, confirmPassword } = formFields;
 
+	const resetFormFields = () => {
+		setformFields(defaultFormfields);
+	};
+
+	const handleSubmit = async (event) => {
+		event.preventDefault();
+
+		if (password !== confirmPassword) {
+			alert('passwords do not match');
+
+			return;
+		}
+
+		try {
+			const { user } = await createAuthUserWithEmailAndPassword(
+				email,
+				password
+			);
+
+			user.displayName = displayName;
+			await createUserDocumentFromAuth(user);
+			resetFormFields();
+		} catch (error) {
+			if (error.code === 'auth/email-already-in-use') {
+				alert('Cannot create user, email already in use');
+			} else {
+				console.error('user creation ecountered an erroe', error);
+			}
+		}
+	};
+
 	const handleChange = (event) => {
 		const { name, value } = event.target;
 
@@ -20,7 +56,7 @@ const SignUpForm = () => {
 	return (
 		<div>
 			<h1>Sign up with your email and password</h1>
-			<form onSubmit={() => {}}>
+			<form onSubmit={handleSubmit}>
 				<label>Display Name</label>
 				<input
 					type='text'
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,9 +1,9 @@
 import { initializeApp } from 'firebase/app';
 import {
 	getAuth,
-	signInWithRedirect,
 	signInWithPopup,
 	GoogleAuthProvider,
+	createUserWithEmailAndPassword,
 } from 'firebase/auth';
 import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
 
@@ -30,6 +30,8 @@ export const signInWithGooglePopup = () =>
 export const db = getFirestore();
 
 export const createUserDocumentFromAuth = async (userAuth) => {
+	if (!userAuth) return;
+
 	const userDocRef = doc(db, 'users', userAuth.uid);
 	const userSnapshot = await getDoc(userDocRef);
 	console.log(userSnapshot);
@@ -53,4 +55,9 @@ export const createUserDocumentFromAuth = async (userAuth) => {
 	}
 
 	return userDocRef;
+};
+
+export const createAuthUserWithEmailAndPassword = async (email, password) => {
+	if (!email || !password) return;
+	return await createUserWithEmailAndPassword(auth, email, password);
 };
