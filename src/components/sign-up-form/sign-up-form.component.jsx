0ad57f14121a89ef86ebc59a9a53c4267295b8ba
Industrylaud <mrfros22@gmail.com>
2023-05-25 21:10:34 +0200

    Add button component

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react';
-
 import {
 	createAuthUserWithEmailAndPassword,
 	createUserDocumentFromAuth,
 } from '../../utils/firebase/firebase.utils';
 
 import FormInput from '../form-input/form-input.component';
+import Button from '../button/button.component';
+import './sign-up-form.styles.scss';
 
 const defaultFormfields = {
 	displayName: '',
@@ -56,8 +57,9 @@ const SignUpForm = () => {
 	};
 
 	return (
-		<div>
-			<h1>Sign up with your email and password</h1>
+		<div className='sign-up-container'>
+			<h2>Don't have an account?</h2>
+			<span>Sign up with your email and password</span>
 			<form onSubmit={handleSubmit}>
 				<FormInput
 					label='Display Name'
@@ -95,7 +97,9 @@ const SignUpForm = () => {
 					value={confirmPassword}
 				/>
 
-				<button type='submit'>Sig Up</button>
+				<Button buttonType='inverted' type='submit'>
+					Sign Up
+				</Button>
 			</form>
 		</div>
 	);
