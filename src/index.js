bee2c4f2f9692559906537b90156cc76846902f2
Industrylaud <mrfros22@gmail.com>
2023-06-20 20:18:10 +0200

    Add and use store

--- src/index.js
+++ src/index.js
@@ -8,20 +8,24 @@ import { CartProvider } from './context/cart.context';
 import { CategoriesProvider } from './context/categories.context';
 import { UserProvider } from './context/user.context';
 
+import { Provider } from 'react-redux';
 import './index.scss';
+import { store } from './store/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <UserProvider>
-        <CategoriesProvider>
-          <CartProvider>
-            <App />
-          </CartProvider>
-        </CategoriesProvider>
-      </UserProvider>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <UserProvider>
+          <CategoriesProvider>
+            <CartProvider>
+              <App />
+            </CartProvider>
+          </CategoriesProvider>
+        </UserProvider>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
 
