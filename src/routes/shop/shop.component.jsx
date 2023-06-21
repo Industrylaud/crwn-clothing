44ed46640a26d808c874ff471fd0e24c233d03f0
Industrylaud <mrfros22@gmail.com>
2023-06-21 20:26:35 +0200

    use categories reducer instead of context

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,11 +1,26 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { Route, Routes } from 'react-router-dom';
 
 import CategoriesPreview from '../categories-preview/categories-preview.component';
 import Category from '../category/category.component';
 
+import { setCategoriesMap } from '../../store/categories/category.action';
+import { getCategoriesAndDocuments } from './../../utils/firebase/firebase.utils';
+
 import './shop.styles.scss';
 
 const Shop = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const getCategoriesMap = async () => {
+      const categoryMap = await getCategoriesAndDocuments();
+      dispatch(setCategoriesMap(categoryMap));
+    };
+    getCategoriesMap();
+  }, [dispatch]);
+
   return (
     <Routes>
       <Route index element={<CategoriesPreview />} />
