54666dc0faa226721632750f7157b3f066e66494
Industrylaud <mrfros22@gmail.com>
2023-10-01 19:12:52 +0200

    refactor

--- src/store/categories/category.selector.js
+++ src/store/categories/category.selector.js
@@ -1,4 +1,4 @@
-import { createSelector } from 'reselect';
+import {createSelector} from 'reselect';
 
 const selectCategoryReducer = (state) => state.categories;
 
@@ -11,7 +11,7 @@ export const selectCategoriesMap = createSelector(
   [selectCategories],
   (categories) =>
     categories.reduce((acc, category) => {
-      const { title, items } = category;
+      const {title, items} = category;
       acc[title.toLowerCase()] = items;
       return acc;
     }, {})
